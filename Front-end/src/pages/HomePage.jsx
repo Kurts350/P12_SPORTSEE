d20969c53d1c7156f884fd006df012f326699d0b
Biaguy Niakate <n.biaguy@gmail.com>
2025-01-09 15:11:13 +0100

    Ajout du style aux bouttons  profil de la home page

--- Front-end/src/pages/HomePage.jsx
+++ Front-end/src/pages/HomePage.jsx
@@ -7,8 +7,20 @@ flex-direction: row;
 gap: 20px;
 `;
 
+const Button = styled.button`
+padding: 15px 20px;
+background-color: black;
+color: white;
+border-radius: 25px;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border: none;
+&:hover {
+  background-color: red;
+  cursor: pointer;
+  transition: 0.3s;
 
-
+}
+`;
 
 
 function HomePage() {
@@ -24,8 +36,8 @@ function HomePage() {
     <>
       <h1>Accueil</h1>
       <ButtonContainer>
-        <button onClick={showProfil12}>Profil 12</button>
-        <button onClick={showProfil18}>Profil 18</button>
+        <Button onClick={showProfil12}>Profil 12</Button>
+        <Button onClick={showProfil18}>Profil 18</Button>
       </ButtonContainer>
     </>
   );
